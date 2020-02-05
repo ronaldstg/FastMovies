63af12e9cece5ff4002025025e88eb417d676e4a
Ronald de Souza <ronald.souza.stg@gmail.com>
2020-02-04 23:56:25 -0300

    Setting up layout and custom views to show top rated movies

---
+++ FastMovies/Home/View/MoviesCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  MoviesCollectionViewCell.swift
+//  FastMovies
+//
+//  Created by Ronald on 04/02/20.
+//  Copyright © 2020 ronald. All rights reserved.
+//
+
+import UIKit
+
+class MoviesCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var posterImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    
+    
+    override func prepareForReuse() {
+        //
+    }
+    
+}
---
+++ FastMovies/Home/ViewController/TopRatedCollectionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TopRatedCollectionViewController.swift
+//  FastMovies
+//
+//  Created by Ronald on 04/02/20.
+//  Copyright © 2020 ronald. All rights reserved.
+//
+
+import UIKit
+
+class TopRatedCollectionViewController: UIViewController {
+
+    @IBOutlet weak var TopRatedCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
